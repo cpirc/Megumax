cb240caf4339193496005c294a4dca6aa0092fcb
Manik Charan <mkchan2951@gmail.com>
2020-05-23 21:45:30 +0530

    Cleanup and optimize eval

--- src/search/mcts/eval.cpp
+++ src/search/mcts/eval.cpp
@@ -1,49 +1,62 @@
 #include "eval.h"
 #include "pst.h"
 
+using libchess::Bitboard;
+using libchess::Color;
+using libchess::PieceType;
+using libchess::Position;
+using libchess::Square;
+
+namespace constants = libchess::constants;
+
 namespace megumax {
 
 constexpr int piece_values[] = {100, 300, 325, 500, 900, 100000};
 
-int count_piece(const libchess::Position& pos, const libchess::PieceType pt) {
-    return pos.piece_type_bb(pt, libchess::constants::WHITE).popcount() +
-           pos.piece_type_bb(pt, libchess::constants::BLACK).popcount();
+int count_piece(const Position& pos, const PieceType pt) {
+    return pos.piece_type_bb(pt, constants::WHITE).popcount() +
+           pos.piece_type_bb(pt, constants::BLACK).popcount();
 }
 
-int phase(const libchess::Position& pos) {
+int phase(const Position& pos) {
     int phase = 24;
-    phase -= count_piece(pos, libchess::constants::KNIGHT);
-    phase -= count_piece(pos, libchess::constants::BISHOP);
-    phase -= 2 * count_piece(pos, libchess::constants::ROOK);
-    phase -= 4 * count_piece(pos, libchess::constants::QUEEN);
+    phase -= count_piece(pos, constants::KNIGHT);
+    phase -= count_piece(pos, constants::BISHOP);
+    phase -= 2 * count_piece(pos, constants::ROOK);
+    phase -= 4 * count_piece(pos, constants::QUEEN);
     return (phase * 256 + 12) / 24;
 }
 
-int eval(const libchess::Position& pos) {
+int eval(const Position& pos) {
     int score = 0;
     int mg_score = 0;
     int eg_score = 0;
-    auto occupancy = pos.occupancy_bb();
 
-    while (occupancy) {
-        const auto sq = occupancy.forward_bitscan();
-        occupancy.forward_popbit();
+    for (Color color : constants::COLORS) {
+        for (PieceType piece_type : constants::PIECE_TYPES) {
+            Bitboard piece_type_bb = pos.piece_type_bb(piece_type, color);
 
-        const auto piece = pos.piece_on(sq);
-        const auto colour = piece->color();
-        const int mul = colour == libchess::constants::WHITE ? 1 : -1;
+            // Material
+            score += piece_values[piece_type.value()] * piece_type_bb.popcount();
 
-        // Material
-        score += piece_values[piece.value().type()] * mul;
+            while (piece_type_bb) {
+                Square piece_sq = piece_type_bb.forward_bitscan();
+                piece_type_bb.forward_popbit();
 
-        // PST
-        if (colour == libchess::constants::WHITE) {
-            mg_score += pst_mg(piece.value().type(), sq);
-            eg_score += pst_eg(piece.value().type(), sq);
-        } else {
-            mg_score -= pst_mg(piece.value().type(), sq.flipped());
-            eg_score -= pst_eg(piece.value().type(), sq.flipped());
+                // PST
+                if (color == constants::WHITE) {
+                    mg_score += pst_mg(piece_type, piece_sq);
+                    eg_score += pst_eg(piece_type, piece_sq);
+                } else {
+                    mg_score += pst_mg(piece_type, piece_sq.flipped());
+                    eg_score += pst_eg(piece_type, piece_sq.flipped());
+                }
+            }
         }
+
+        score = -score;
+        mg_score = -mg_score;
+        eg_score = -eg_score;
     }
 
     // Phase
@@ -51,7 +64,7 @@ int eval(const libchess::Position& pos) {
     score += ((mg_score * (256 - p)) + (eg_score * p)) / 256;
 
     // Return from side to move's pov
-    if (pos.side_to_move() != libchess::constants::WHITE) {
+    if (pos.side_to_move() != constants::WHITE) {
         score = -score;
     }
 
